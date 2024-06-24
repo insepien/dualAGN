3546d8c9eddfa39d074d10529d427ccbbf88a6f1
insepien <my.do99@gmail.com>
2024-06-24 11:57:45 -0700

    move data locally, add script to fit large frame+plot 1d

--- fit/fit.bash
+++ fit/fit.bash
@@ -6,10 +6,24 @@ log_file="fit_log.txt"
 # Clear the log file before running the script
 > "$log_file"
 
-files=$(ls ../cutouts/data)
+# files=$(ls ../cutouts/data)
 
-for file in $files; do
+files=(
+    'J0328-0710.fits'
+    'J0400-0652.fits'
+    'J0752+2019.fits'
+    'J0813+0905.fits'
+    'J0820+1801.fits'
+    'J0821+1450.fits'
+    'J0859+1001.fits'
+    'J0901+1815.fits'
+    'J0906+1840.fits'
+    'J0912+0148.fits'
+    'J0918+1100.fits'
+)
+
+for file in "${files[@]}"; do
     # Redirect both stdout and stderr to the log file
-    python3 fit.py --inFile "$file" >> "$log_file" 2>&1
-    echo "Completed processing file: $file"
+    python3 fit_allmod.py --inFile "$file" >> "$log_file" 2>&1
+    echo "Done: $file"
 done
