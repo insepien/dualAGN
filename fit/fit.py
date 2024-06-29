5c93af0a607317dc10df2dba51e137fe2e3b826d
insepien <my.do99@gmail.com>
2024-06-29 11:57:03 -0700

    make script for repeated class/function, add fitstat1d+positions to plot

--- fit/fit.py
+++ fit/fit.py
@@ -4,10 +4,10 @@ import os
 import pyimfit
 from astropy.io import fits
 import matplotlib.pyplot as plt
-import pandas as pd
 from tqdm import tqdm
 import logging
 import glob
+from modelComponents import makeModelDict
 
 medium_font_size = 14 
 plt.rcParams['font.size'] = medium_font_size
@@ -48,35 +48,6 @@ def find_sky(image, objectName, args):
         ax[0].set_ylabel('number of agns') 
         fig.savefig("skyHist_"+objectName+".jpg")
     return sky
-
-
-def makeModelDict(PA_ss, ell_ss, n_ss, I_ss, r_ss, Itot,
-                 PA_lim, ell_lim, I_lim,  Iss_lim, rss_lim, Itot_lim,
-                 sigma, sigma_lim, Isky, Isky_lim,
-                 h1,h2,h_lim,alpha,alpha_lim):
-    """Return Sersic, PSF, and Gaussian model parameter dictionary"""
-    # Sersic
-    """sersic = {'PA': [PA_ss, PA_lim[0],PA_lim[1]], 'ell_bulge': [ell_ss, ell_lim[0],ell_lim[1]], 'n': [n_ss, 'fixed'],
-    'I_e': [I_ss, Iss_lim[0],Iss_lim[1]], 'r_e': [r_ss, rss_lim[0],rss_lim[1]]}"""
-    sersic = {'PA': [PA_ss, PA_lim[0],PA_lim[1]], 'ell_bulge': [ell_ss, ell_lim[0],ell_lim[1]], 'n': [n_ss, 0, 10],
-    'I_e': [I_ss, Iss_lim[0],Iss_lim[1]], 'r_e': [r_ss, rss_lim[0],rss_lim[1]]}
-    sersic_dict = {'name': "Sersic", 'label': "bulge", 'parameters': sersic}
-    # PSF
-    psf = {'I_tot' : [Itot, Itot_lim[0], Itot_lim[1]]}
-    psf_dict = {'name': "PointSource", 'label': "psf", 'parameters': psf}
-    """psf = {'I_tot' : [Itot, Itot_lim[0], Itot_lim[1]], 'PA':[PA_ss, PA_lim[0],PA_lim[1]] }
-    psf_dict = {'name': "PointSourceRot", 'label': "psf", 'parameters': psf}"""
-    # flat bar
-    flatbar = {'PA':[PA_ss, PA_lim[0],PA_lim[1]], 'ell':[ell_ss, ell_lim[0],ell_lim[1]],
-               'deltaPA_max':[PA_ss, PA_lim[0],PA_lim[1]], 'I_0':[I_ss, Iss_lim[0],Iss_lim[1]],
-               'h1':[h1, h_lim[0],h_lim[1]], 'h2':[h2, h_lim[0],h_lim[1]], 
-               'r_break':[r_ss, rss_lim[0],rss_lim[1]], 'alpha':[alpha,alpha_lim[0],alpha_lim[1]]}
-    flatbar_dict = {'name': "FlatBar", 'label': "flat_bar", 'parameters':flatbar}
-    # Exponential
-    exponential = {'PA': [PA_ss, PA_lim[0],PA_lim[1]], 'ell': [ell_ss, ell_lim[0],ell_lim[1]], 
-                   'I_0': [I_ss, Iss_lim[0],Iss_lim[1]], 'h': [h1, h_lim[0],h_lim[1]]}
-    exp_dict = {'name': "Exponential", 'label': "disk", 'parameters':exponential}
-    return sersic_dict, psf_dict, flatbar_dict, exp_dict
 
 
 def galaxy_funcdict(X0, Y0, X1, Y1, Xss0, Yss0, Xss1, Yss1, 
--- fit/makePlotComps.py
+++ fit/makePlotComps.py
@@ -2,44 +2,10 @@ import numpy as np
 import pickle
 import os
 import pyimfit
-from astropy.io import fits
-import matplotlib.pyplot as plt
-import pandas as pd
-from photutils.isophote import Ellipse, EllipseGeometry, IsophoteList
-from photutils.isophote import EllipseSample, Isophote
+from photutils.isophote import Ellipse, EllipseGeometry
 from photutils.isophote.sample import CentralEllipseSample
 from photutils.isophote.fitter import CentralEllipseFitter
-from matplotlib.ticker import FormatStrFormatter
-from matplotlib.backends.backend_pdf import PdfPages
-
-
-def makeModelDict(PA_ss, ell_ss, n_ss, I_ss, r_ss, Itot,
-                 PA_lim, ell_lim, I_lim,  Iss_lim, rss_lim, Itot_lim,
-                 sigma, sigma_lim,
-                 h1,h2,h_lim,alpha,alpha_lim):
-    """Return Sersic, PSF, and Gaussian model parameter dictionary"""
-    # Sersic
-    """sersic = {'PA': [PA_ss, PA_lim[0],PA_lim[1]], 'ell_bulge': [ell_ss, ell_lim[0],ell_lim[1]], 'n': [n_ss, 'fixed'],
-    'I_e': [I_ss, Iss_lim[0],Iss_lim[1]], 'r_e': [r_ss, rss_lim[0],rss_lim[1]]}"""
-    sersic = {'PA': [PA_ss, PA_lim[0],PA_lim[1]], 'ell_bulge': [ell_ss, ell_lim[0],ell_lim[1]], 'n': [n_ss, 0, 10],
-    'I_e': [I_ss, Iss_lim[0],Iss_lim[1]], 'r_e': [r_ss, rss_lim[0],rss_lim[1]]}
-    sersic_dict = {'name': "Sersic", 'label': "bulge", 'parameters': sersic}
-    # PSF
-    psf = {'I_tot' : [Itot, Itot_lim[0], Itot_lim[1]]}
-    psf_dict = {'name': "PointSource", 'label': "psf", 'parameters': psf}
-    """psf = {'I_tot' : [Itot, Itot_lim[0], Itot_lim[1]], 'PA':[PA_ss, PA_lim[0],PA_lim[1]] }
-    psf_dict = {'name': "PointSourceRot", 'label': "psf", 'parameters': psf}"""
-    # flat bar
-    flatbar = {'PA':[PA_ss, PA_lim[0],PA_lim[1]], 'ell':[ell_ss, ell_lim[0],ell_lim[1]],
-               'deltaPA_max':[PA_ss, PA_lim[0],PA_lim[1]], 'I_0':[I_ss, Iss_lim[0],Iss_lim[1]],
-               'h1':[h1, h_lim[0],h_lim[1]], 'h2':[h2, h_lim[0],h_lim[1]], 
-               'r_break':[r_ss, rss_lim[0],rss_lim[1]], 'alpha':[alpha,alpha_lim[0],alpha_lim[1]]}
-    flatbar_dict = {'name': "FlatBar", 'label': "flat_bar", 'parameters':flatbar}
-    # Exponential
-    exponential = {'PA': [PA_ss, PA_lim[0],PA_lim[1]], 'ell': [ell_ss, ell_lim[0],ell_lim[1]], 
-                   'I_0': [I_ss, Iss_lim[0],Iss_lim[1]], 'h': [h1, h_lim[0],h_lim[1]]}
-    exp_dict = {'name': "Exponential", 'label': "disk", 'parameters':exponential}
-    return sersic_dict, psf_dict, flatbar_dict, exp_dict
+from modelComponents import modelComps, makeModelDict
 
 
 def make_model_components(config,imshape):
@@ -74,6 +40,7 @@ def profile_1D(semiA,image,PA=180,ell=0.5):
         return isolist
 
 def make_data_isophotes(data,sma,midFrame):
+    """make 1d profile of data"""
     isolist_data = profile_1D(semiA=sma,image=data)
     # discard first isophote and make new
     isolist_data = isolist_data[1:]
@@ -92,55 +59,6 @@ def plot_isophotes(ax,isolist,num_aper=10):
         iso = isolist.get_closest(sma)
         x, y, = iso.sampled_coordinates()
         ax.plot(x, y, color='white',linewidth="0.5")
-
-
-def plot_model_components(comp_ims,comp_names,isolist_comps,plot_iso=False):
-    """plot 2D model components and check residual with model image"""
-    ncom = len(comp_names)
-    fig,ax = plt.subplots(nrows=1,ncols=ncom+1, figsize=(ncom*3+3,3))
-    im = [ax[i].imshow(comp_ims[i],norm='symlog') for i in range(ncom)]
-    [ax[i].set_title(comp_names[i]) for i in range(ncom)]
-    im.append(ax[-1].imshow(np.sum(comp_ims[:-1],axis=0)-comp_ims[-1],norm='symlog'))
-    ax[-1].set_title("model-comps")
-    [fig.colorbar(im[i], ax=ax[i], shrink=0.7).ax.yaxis.set_major_formatter(FormatStrFormatter('%.2f')) for i in range(len(ax))]
-    if plot_iso:
-        for i in range(len(isolist_comps)):
-             plot_isophotes(ax[i],isolist_comps[i],num_aper=5)
-    fig.tight_layout();
-
-
-class modelComps:
-    def __init__(self, modelname, comp_im, comp_pos, comp_name,fit_result):
-        self.model_name = modelname
-        self.comp_im = comp_im
-        self.comp_pos = comp_pos
-        self.comp_name = comp_name
-        self.fit_result = fit_result
-        self.iso_comp = None
-
-    def make_model_isophotes(self,isolist_data):
-        isolist_comps=[]
-        midf = self.comp_im[0].shape[0]//2
-        circ = [self.comp_name[i]=='psf' for i in range(len(self.comp_name))]
-        for i in range(len(self.comp_name)):
-            isolist_ = []
-            for iso in isolist_data[1:]:
-                g = iso.sample.geometry
-                ell = 0 if circ[i] else g.eps
-                gn = EllipseGeometry(g.x0,g.y0, g.sma, ell, g.pa)
-                sample = EllipseSample(self.comp_im[i],g.sma,geometry=gn)
-                sample.update()
-                iso_ = Isophote(sample,0,True,0)
-                isolist_.append(iso_)
-            isolist = IsophoteList(isolist_)
-            g = EllipseGeometry(midf,midf, 0.0, 0., 0.)
-            sample = CentralEllipseSample(self.comp_im[i], 0., geometry=g)
-            fitter = CentralEllipseFitter(sample)
-            center = fitter.fit()
-            isolist.append(center)
-            isolist.sort()
-            isolist_comps.append(isolist)
-        self.iso_comp = isolist_comps
 
 
 if __name__=="__main__":
