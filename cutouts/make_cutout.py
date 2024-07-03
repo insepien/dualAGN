fix path to absolute path in scripts, make masking script, fit 128 masked

cutouts/make_cutout.py
@@ -6,11 +6,12 @@ from astropy.nddata import Cutout2D
 import pandas as pd
 import numpy as np
 import os
+import glob
 
 def download_image_save_cutout(originalfile, position, size, cutoutfile):
     """Returns a cutout of agn from original exp.fits"""
     # Load the image and the WCS
-    hdu = fits.open(originalfile)[0]
+    hdu = fits.open(os.path.expanduser(originalfile))[0]
     wcs = WCS(hdu.header)
     # Make the cutout, including the WCS
     cutout = Cutout2D(hdu.data, position=position, size=size, wcs=wcs)
@@ -28,22 +29,31 @@ if __name__ == "__main__":
         """
         script to make cutout
         """), formatter_class=RawDescriptionHelpFormatter)
-    parser.add_argument("--outDir", default="box", type=str, help="output directory")
+    #parser.add_argument("--inDir", default="~/raw-data-agn/mos-fits-agn", type=str, help="output directory")
+    parser.add_argument("--outDir", default="~/agn-result/box", type=str, help="output directory")
     parser.add_argument("--cutSize", type=int, help="fits cutout size")
-    parser.add_argument("--inFile", type=str, help="input exposure file")
+    parser.add_argument("--objectName", type=str, help="object name")
+    parser.add_argument("--makeMulti", action= 'store_true')
     args = parser.parse_args()    
     # reading coordinates from catalog
     catalog = pd.read_csv('catalog.txt', names=['name', 'ra', 'dec'], delimiter='\s+')
     coords = [SkyCoord(ra=catalog['ra'].loc[i]*u.deg, dec=catalog['dec'].loc[i]*u.deg) for i in range(len(catalog))]
     catalog['coords'] = coords
     catalog.set_index("name",inplace=True)
-    objectName = args.inFile.split("_")[2]
-    exp_path = "../../agn-data/"+args.inFile
-    # make directory to store cutout of different sizes
-    boxdirname = "boxsize_"+objectName
-    #boxdir = os.mkdir(os.path.join(args.outDir,boxdirname))
+    exp_path = glob.glob(os.path.expanduser("~/raw-data-agn/mos-fits-agn/2020-02-22_J_"+args.objectName+"*.mos.fits"))[0]
 
-    # make cutout of size 80-119 and save
-    for i in np.arange(310,500,10):
-        save_path = os.path.join(args.outDir,boxdirname,objectName+'_'+str(i)+'.fits') 
-        download_image_save_cutout(exp_path, catalog['coords'].loc[objectName], (i,i), save_path)
+    boxdirname = "boxsize_"+args.objectName
+    if args.makeMulti:
+        # make directory to store cutout of different sizes
+        boxdir = os.mkdir(os.path.join(args.outDir,boxdirname))
+        # make cutout of different sizes and save
+        for i in np.arange(100,500,10):
+            save_path = os.path.join(args.outDir,boxdirname,args.objectName+'_'+str(i)+'.fits') 
+            download_image_save_cutout(exp_path, catalog['coords'].loc[args.objectName], (i,i), save_path)
+    
+
+    if args.cutSize:
+        save_path = os.path.join(args.outDir,boxdirname,args.objectName+'_'+str(args.cutSize)+'.fits')
+        download_image_save_cutout(exp_path, catalog['coords'].loc[args.objectName], (args.cutSize, args.cutSize), save_path)
+
+    print("Finished")

fit/plot_fit_with1d.py
@@ -24,7 +24,7 @@ def plot_isophotes(ax,isolist,num_aper=10):
         ax.plot(x, y, color='white',linewidth="0.5")
 
 
-def plot_model_components(pdf,comp_ims,comp_names,comp_pos,isolist_comps,plot_iso=False):
+def plot_model_components(pdf,comp_ims,comp_names,comp_pos,isolist_comps,args):
     """plot 2D model components and check residual with model image"""
     ncom = len(comp_names)
     fig,ax = plt.subplots(nrows=1,ncols=ncom+1, figsize=(14,3))
@@ -34,7 +34,7 @@ def plot_model_components(pdf,comp_ims,comp_names,comp_pos,isolist_comps,plot_is
     im.append(ax[-1].imshow(np.sum(comp_ims[:-1],axis=0)-comp_ims[-1],norm='symlog'))
     ax[-1].set_title("model-comps")
     [fig.colorbar(im[i], ax=ax[i], shrink=0.7).ax.yaxis.set_major_formatter(FormatStrFormatter('%.2f')) for i in range(len(ax))]
-    if plot_iso:
+    if args.plotIso:
         for i in range(len(isolist_comps)):
              plot_isophotes(ax[i],isolist_comps[i],num_aper=5)
     fig.tight_layout()
@@ -90,10 +90,11 @@ if __name__=="__main__":
         """
         script to plot fit results
         """), formatter_class=RawDescriptionHelpFormatter)
-    parser.add_argument("--inDir", type=str, default="~/agn-result/fit", help="input directory of fit result files")
+    parser.add_argument("--inDir", type=str, default="~/agn-result/fit/components", help="input directory of fit result files")
     parser.add_argument("--inFile", type=str, help="fit result file")
     parser.add_argument("--outDir", type=str, default="~/agn-result/fit", help="directory for fit results")
     parser.add_argument("--outFile", type=str, help="output file")
+    parser.add_argument("--plotIso", action='store_true')
     args = parser.parse_args()
     
     # load fit component file
@@ -121,5 +122,5 @@ if __name__=="__main__":
                             isolist_data=isolist_agn,isolist_comps=model.iso_comp,
                             comp_names=model.comp_name, fs=model.fit_result.fitStat)
             plot_model_components(pdf,comp_ims=model.comp_im,comp_names=model.comp_name, comp_pos=model.comp_pos,
-                                  isolist_comps=model.iso_comp,plot_iso=True)
+                                  isolist_comps=model.iso_comp,args=args)
     print("Done: ", objectName)