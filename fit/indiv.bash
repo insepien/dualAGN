d6a665bb8706c93555110dfeb4eb87a278b5d6bd
insepien <my.do99@gmail.com>
2024-10-09 13:46:48 -0700

    1 line fixes for cutouts and component making

--- fit/indiv.bash
+++ fit/indiv.bash
@@ -6,8 +6,8 @@ log_file="indiv_log.txt"
 # Clear the log file before running the script
 > "$log_file"
 
-objectName='J0752+2019'
+objectName='J1222-0007'
 
-#python3 fit.py --oname "$objectName" >> "$log_file" 2>&1
-#python3 makePlotComps.py --oname "$objectName" --sma 30 >> "$log_file" 2>&1
+python3 fit.py --oname "$objectName" >> "$log_file" 2>&1
+python3 makePlotComps.py --oname "$objectName" --sma 15 >> "$log_file" 2>&1
 python3 plot_fit_with1d.py --oname "$objectName" >> "$log_file" 2>&1
