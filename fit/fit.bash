7e27b1a812bfb2bdbcfaff94c61e7025946012a0
insepien <my.do99@gmail.com>
2024-10-15 10:46:18 -0700

    change 1d chi to reduced

--- fit/fit.bash
+++ fit/fit.bash
@@ -6,10 +6,10 @@ log_file="fit_log.txt"
 # Clear the log file before running the script
 > "$log_file"
 
-files=$(ls ~/agn-result/box/final_cut | sort)
+files=$(ls ~/agn-result/box/200 | sort)
 
 #for f in $(echo "$files" | head -n 5); do
-for f in $(echo "$files" | sed -n '20,31p'); do
+for f in $(echo "$files" | sed -n '31,43p'); do
     base_name=$(basename "$f" .fits)
     objectName=${base_name:0:10}
     python3 fit.py --oname "$objectName" >> "$log_file" 2>&1
--- fit/plot_fit.bash
+++ fit/plot_fit.bash
@@ -9,10 +9,10 @@ log_file="plot_fit_log.txt"
 files=$(ls ~/agn-result/fit/final_fit | sort)
 
 #for f in $(echo "$files" | head -n 5); do
-for f in $(echo "$files" | sed -n '20,31p'); do
+for f in $(echo "$files" | sed -n '31,43p'); do
     base_name=$(basename "$f" .fits)
     objectName=${base_name:0:10}
-    python3 makePlotComps.py --oname "$objectName" >> "$log_file" 2>&1
+    python3 makePlotComps.py --oname "$objectName" --sma 20 >> "$log_file" 2>&1
     python3 plot_fit_with1d.py --oname "$objectName" >> "$log_file" 2>&1
     echo "Done: $objectName"
 done
