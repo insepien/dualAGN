f8551d521e35439e38f9fc9e9cb828c2d2831c68
insepien <my.do99@gmail.com>
2024-10-21 14:53:13 -0700

    minor plot and fit changes

--- fit/fit.bash
+++ fit/fit.bash
@@ -9,7 +9,7 @@ log_file="fit_log.txt"
 files=$(ls ~/agn-result/box/200 | sort)
 
 #for f in $(echo "$files" | head -n 5); do
-for f in $(echo "$files" | sed -n '31,43p'); do
+for f in $(echo "$files" | sed -n '1,6p'); do
     base_name=$(basename "$f" .fits)
     objectName=${base_name:0:10}
     python3 fit.py --oname "$objectName" >> "$log_file" 2>&1
--- fit/fit.py
+++ fit/fit.py
@@ -79,7 +79,7 @@ def galaxy_funcdict(X0, Y0, X1, Y1, Xss0, Yss0, Xss1, Yss1,
         "sersic+sersic":[funcset_dict_serser0],
         
         "psf+sersic,psf": [funcset_dict_psfser0,funcset_dict_psf1],
-        "psf+sersic,sersic": [funcset_dict_sersic1,funcset_dict_psfser0],
+        "psf+sersic,sersic": [funcset_dict_psfser0,funcset_dict_sersic1],
         "2psf+sersic": [funcset_dict_psfser0,funcset_dict_psfser1],
         "sersic+sersic,sersic+sersic": [funcset_dict_serser0, funcset_dict_serser1],
         
