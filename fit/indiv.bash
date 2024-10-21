511f315feb6d7d2df43f2b97b7dbdcfb3a6c7864
insepien <my.do99@gmail.com>
2024-10-21 13:59:03 -0700

    neat up plot 1d script

--- fit/indiv.bash
+++ fit/indiv.bash
@@ -8,6 +8,6 @@ log_file="indiv_log.txt"
 
 objectName='J0323+0018'
 
-python3 fit.py --oname "$objectName" >> "$log_file" 2>&1
-python3 makePlotComps.py --oname "$objectName" --sma 20 >> "$log_file" 2>&1
+#python3 fit.py --oname "$objectName" >> "$log_file" 2>&1
+#python3 makePlotComps.py --oname "$objectName" --sma 20 >> "$log_file" 2>&1
 python3 plot_fit_with1d.py --oname "$objectName" >> "$log_file" 2>&1
