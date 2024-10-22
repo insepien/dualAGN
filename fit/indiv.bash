e59cceb876ea71ce6616d000ca4d00c2933a1fcc
insepien <my.do99@gmail.com>
2024-10-22 08:46:55 -0700

    change back to egain

--- fit/indiv.bash
+++ fit/indiv.bash
@@ -6,8 +6,8 @@ log_file="indiv_log.txt"
 # Clear the log file before running the script
 > "$log_file"
 
-objectName='J0400-0652'
+objectName='J0752+2019'
 
-python3 fit.py --oname "$objectName" >> "$log_file" 2>&1
+#python3 fit.py --oname "$objectName" >> "$log_file" 2>&1
 python3 makePlotComps.py --oname "$objectName" --sma 10 >> "$log_file" 2>&1
 python3 plot_fit_with1d.py --oname "$objectName" >> "$log_file" 2>&1
