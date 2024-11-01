5dfb1bb3c7495961c2b546609022dd97fa0bc1aa
insepien <my.do99@gmail.com>
2024-11-01 11:47:36 -0700

    change bash to include pa guess and change directories

--- fit/fit.bash
+++ fit/fit.bash
@@ -6,12 +6,16 @@ log_file="fit_log.txt"
 # Clear the log file before running the script
 > "$log_file"
 
-files=$(ls ~/agn-result/box/200 | sort)
+mapfile -t files < <(ls ~/research-data/agn-result/box/200 | sort)
+files=("${files[@]:18}")
+PAs=(70 90 100 170 160 10 170)
+length=${#PAs[@]}
 
-#for f in $(echo "$files" | head -n 5); do
-for f in $(echo "$files" | sed -n '1,6p'); do
+for ((i=0; i<length; i++)); do
+    f=${files[i]}
+    pa=${PAs[i]} 
     base_name=$(basename "$f" .fits)
     objectName=${base_name:0:10}
-    python3 fit.py --oname "$objectName" >> "$log_file" 2>&1
+    #python3 fit.py --oname "$objectName" --PA "$pa" --outDir "~/research-data/agn-result/fit/test_fit_masked/masked_fit" >> "$log_file" 2>&1
     echo "Done: $objectName"
 done
