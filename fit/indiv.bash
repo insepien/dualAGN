9d484af7a6dc640401a4c61e6af60fbcecdc8845
insepien <my.do99@gmail.com>
2024-12-27 15:02:07 -0800

    change some plotting details

--- fit/indiv.bash
+++ fit/indiv.bash
@@ -6,8 +6,18 @@ log_file="indiv_log.txt"
 # Clear the log file before running the script
 > "$log_file"
 
-objectName='J0918+1100'
+objectName='J0323+0018'
 
-python3 fit.py --inDir "/home/insepien/research-data/agn-result/fit/fit_masked_n.3to6/" --inFile "J0918+1100.fits" --oname "$objectName" --original --outDir "/home/insepien/research-data/agn-result/fit/fit_masked_n.3to6/masked_fit" --PA 160 --ELL 0.5 --RE 3  >> "$log_file" 2>&1
-python3 makePlotComps.py --oname "$objectName" --sma 10 --inDir "/home/insepien/research-data/agn-result/fit/fit_masked_n.3to6/masked_fit" --outDir "/home/insepien/research-data/agn-result/fit/fit_masked_n.3to6/masked_fit_comp" >> "$log_file" 2>&1
-python3 plot_fit_with1d.py --oname "$objectName" --inDir "/home/insepien/research-data/agn-result/fit/fit_masked_n.3to6/masked_fit_comp" --outDir "/home/insepien/research-data/agn-result/fit/fit_masked_n.3to6"  >> "$log_file" 2>&1
+python3 fit.py --inDir "/home/insepien/research-data/agn-result/fit/fit_masked_n.3to6/masked_image_SS/" \
+--inFile "${objectName}.fits" --oname "$objectName" \
+--outDir "/home/insepien/research-data/agn-result/fit/fit_masked_n.3to6/masked_fit" \
+--PA 130 --ELL 0.5 --RE 3  >> "$log_file" 2>&1
+
+python3 makePlotComps.py --oname "$objectName" --sma 10 \
+--inDir "/home/insepien/research-data/agn-result/fit/fit_masked_n.3to6/masked_fit" \
+--outDir "/home/insepien/research-data/agn-result/fit/fit_masked_n.3to6/masked_fit_comp" >> "$log_file" 2>&1
+
+python3 plot_fit_with1d.py --oname "$objectName" \
+--inDir "/home/insepien/research-data/agn-result/fit/fit_masked_n.3to6/masked_fit_comp" \
+--outDir "/home/insepien/research-data/agn-result/fit/fit_masked_n.3to6"  >> "$log_file" 2>&1
+
