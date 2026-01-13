ef82d118bafd5a6f8d597bba402432092cabdec6
insepien <my.do99@gmail.com>
2026-01-13 06:48:23 -0800

    add single sky calculation, detailed doc for cutout script

--- cutouts/make_cutout.py
+++ cutouts/make_cutout.py
@@ -53,9 +53,14 @@ def photsky(data):
     return np.array([data.shape[0], mean, median, std])
 
 def cal_background(on):
-    """calculate sky background as the median of the background level at different cutout sizes"""
+    """calculate sky background as the median of the background level at different cutout sizes
+        returns the best estimate sky level, array res == 10 rows x 4 cols
+                each row = 1 cutout, each column = cutout size, mean, med, std of pix intensity"""
     # load exposure
-    exp_path = glob.glob(os.path.expanduser("~/raw-data-agn/mos-fits-agn/*"+on+"*.fits"))[0]
+    if args.mos_path:
+        exp_path = glob.glob(os.path.expanduser(args.mos_path+"/*"+on+"*.fits"))[0]
+    else:
+        exp_path = glob.glob(os.path.expanduser("~/raw-data-agn/mos-fits-agn/*"+on+"*.fits"))[0]
     # create cutout of size 100 to 1000
     cutouts = [make_cutout(exp_path, catalog['coords'].loc[on], (i,i))[1] for i in np.arange(1,11)*100]
     # calculate background stats for each cutout size
@@ -123,29 +128,54 @@ if __name__ == "__main__":
     from argparse import ArgumentParser, RawDescriptionHelpFormatter
     parser = ArgumentParser(description=(
         """
-        script to make cutout
+        Script usage includes
+        --------------------------------------------------------
+        1. Sky level calculation:
+        - for all target at the same time use 
+                python make_cutout.py --calSky
+        This saves (1) sky level and data used to find sky level in args.skyfull_fname to drive
+                    (2) a clean dict of {oname : best sky est} to args.skyclean_fname to the fit folder
+        - for single target use
+                python make_cutout.py --calSky_single --mos_path [if there is a special to mos.fits file]
+        This just prints out the best estimate sky, which needd to be added manually to the sky.pkl for fitting
+        --------------------------------------------------------
+        2. Make cutout (not masked) of size pix x pix (used in old fits)
+                python make_cutout.py --cutSize [in unit of pix] -- RA [option to cut at some RA, DEC]
+        --------------------------------------------------------
+        3. Make cutout (masked, size in kpc unit)
+        - for single object. default size is 70 kpc, only to view the cutout, to save add --savesingle, to adjust masking use --deblendpix and --sharedpix
+                python make_cutout.py --oname [object name] --makeMaskedFits_kpcbox --single 
+        some notes on adjustments: higher --deblendpix value equals less masking (since the ext sources are not separated from main)
+                                    can include --framesize_kpc [in kpc]
+        - for all objs in loaded sample df stored in var 'magel'
+                python make_cutout.py --oname [object name] --makeMaskedFits_kpcbox
         """), formatter_class=RawDescriptionHelpFormatter)
     parser.add_argument("--outDir", default="~/research-data/agn-result/box/kpcbox", type=str, help="output directory")
-    # args for creating single cutout
     parser.add_argument("--oname", type=str, help="object name")
+    # calculate and save sky level for all targets 
+    parser.add_argument("--calSky", action= 'store_true', help='flag to calculate sky level for all targets and save')
+    parser.add_argument("--skyfull_fname", default="sky_full.pkl", help='name of the file storing the full sky level data')
+    parser.add_argument("--skyclean_fname", default="sky.pkl", help='name of the file storing a clean dict of sky levels for fitting')
+    parser.add_argument("--savePlotSky", action= 'store_true', help="**SAVE** plots of sky level vs cutout size for all targets")
+    # calculate and print sky level for a single target
+    parser.add_argument("--calSky_single", action='store_true',help='flag to calculate sky and print for only 1 target')
+    parser.add_argument("--mos_path", type=str, help='special path to mos.fits folder, if any')
+    # args for creating single cutout of some cutSize [pix]
     parser.add_argument("--cutSize", type=int, help="fits cutout size")
     parser.add_argument("--RA", type=float, help='cutout center RA')
     parser.add_argument("--DEC", type=float, help='cutout center DEC')
-    # for calculating sky levels for all targets and save to df
-    parser.add_argument("--calSky", action= 'store_true')
-    parser.add_argument("--plotSky", action= 'store_true')
-    # for save masked fits, not sky subtracted, same phys size for all targets
-    parser.add_argument("--savefits", action= 'store_true')
-    parser.add_argument("--single", action= 'store_true')
-    parser.add_argument("--savesingle", action= 'store_true')
-    parser.add_argument("--deblendpix", type=float)
-    parser.add_argument("--sharedpix", type=float)
-    parser.add_argument("--framesize_kpc", type=float)
+    # save masked fits, not sky subtracted, same phys size for all targets
+    parser.add_argument("--makeMaskedFits_kpcbox", action= 'store_true')
+    parser.add_argument("--single", action= 'store_true', help='use this flag to make 1 target cutout')
+    parser.add_argument("--savesingle", action= 'store_true', help='use this flag to save the cutout')
+    parser.add_argument("--deblendpix", type=float, default=10, help='min number of connected pix an obj must have to be deblended')
+    parser.add_argument("--sharedpix", type=float,default=10, help='min number of shared pix an obj must have to be detected')
+    parser.add_argument("--framesize_kpc", type=float, default = 70)
 
     args = parser.parse_args()    
 
     # load sample data, will use name and Z
-    magel = pd.read_pickle("/home/insepien/research-data/alpaka/magellan/alpaka_39fits.pkl")
+    magel = pd.read_pickle("/home/insepien/research-data/alpaka/magellan/magel43.pkl")#("/home/insepien/research-data/alpaka/magellan/alpaka_39fits.pkl")
     # load catalog of target coords in FK5
     catalog = pd.read_csv('../cutouts/catalog.txt', names=['name', 'ra', 'dec'], delimiter='\s+')
     coords = [SkyCoord(ra=catalog['ra'].loc[i]*u.deg, dec=catalog['dec'].loc[i]*u.deg, frame="fk5") for i in range(len(catalog))]
@@ -153,27 +183,40 @@ if __name__ == "__main__":
     catalog.set_index("name",inplace=True)
 
     # option to calculate sky background: (1)generate cutout of size 100-1000, (2)sigma-clip and calculate backgrounds, (3) take median of all bckgr
+    # for 1 source
+    if args.calSky_single:
+        skylev, res = cal_background(args.oname)
+        print(skylev)
+
+    # for all sources in sample
     if args.calSky:
         # calculate background level for each target
-        savedata = {}
+        fullskydata = {} # for saving all data
+        cleanskydata = {} # for saving a short version of data for fitting
         for on in magel['Desig'].values:
             skylev, res = cal_background(on)
-            savedata[on] = [skylev, res]
+            fullskydata[on] = [skylev, res]
+            cleanskydata[on] = skylev
         # save background data
-        with open("/home/insepien/research-data/agn-result/box/skylev/sky.pkl","wb") as f:
-            pickle.dump(savedata,f)
-        print("Sky level saved")
-    
-    if args.plotSky:
-        bg = pd.read_pickle("/home/insepien/research-data/agn-result/box/skylev/sky.pkl")
+        with open("/home/insepien/research-data/agn-result/box/skylev/"+args.skyfull_fname,"wb") as f:
+            pickle.dump(fullskydata,f)
+        print("Full sky level file saved")
+        # save the clean sky dict for fitting
+        with open("../fit/"+args.skyclean_fname,"wb") as f:
+            pickle.dump(cleanskydata,f)
+        print("Clean sky level file saved")
+
+    # option to save plot of sky level vs cutout size for each target for sanity check
+    if args.savePlotSky:
+        bg = pd.read_pickle("/home/insepien/research-data/agn-result/box/skylev/"+args.skyfull_fname)
         for on in magel['Desig'].values:
             # save plot for sanity check later
             plot_sky(bg[on][0],bg[on][1])
             print(f"done: {on}")
 
-    # option to generate a cutout (not masked) for a specific object
-    if args.oname and args.cutSize:
-        save_path = os.path.join(args.outDir,args.onam+'_'+str(args.cutSize)+'.fits')
+    # option to generate a cutout (not masked, cut size in pix) for a specific object
+    if args.cutSize:
+        save_path = os.path.join(args.outDir,args.oname+'_'+str(args.cutSize)+'.fits')
         exp_path = glob.glob(os.path.expanduser("~/raw-data-agn/mos-fits-agn/*"+args.oname+"*.fits"))[0]
         if args.RA:
             coords = SkyCoord(ra=args.RA*u.deg, dec=args.DEC*u.deg)
@@ -186,10 +229,10 @@ if __name__ == "__main__":
 
 
     # save cutout fits for all objects with uniform physical size. Image is masked. backgrond is saved in header
-    if args.savefits:
-        if args.single:
+    if args.makeMaskedFits_kpcbox:
+        if args.single: # make a single cutout
             make_masked_cutout(args.oname, deblendpix=args.deblendpix, npix=args.sharedpix,save=args.savesingle)
-        else:
+        else: # make all cutouts
             for on in magel['Desig'].values:
                 make_masked_cutout(on)
                 print(f"done {on}")
